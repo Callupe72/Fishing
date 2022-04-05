4462a29e299b22129c897171d1882fb903bb6ea0
Paul Leduc <paulleduc72@gmail.com>
2022-04-05 16:56:26 +0200

    UPdate

---
+++ FishingDuck/Assets/Scripts/Ducks/ScriptableDucks.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "New Duck", fileName = "New Duck")]
+public class ScriptableDucks : ScriptableObject
+{
+    public Color color;
+    public int points;
+}
